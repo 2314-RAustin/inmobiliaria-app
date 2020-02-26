04378b301d70b8631bc860bcff3ec81e59866676
Roberto E. M. Austin <0315108515@miutt.edu.mx>
2020-02-25 22:15:19 -0800

    Se agrega un AppBar, se utliza react router dom, uso de grid y uso de nuestros templates creados con material ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, {Fragment, Component} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import ListaInmuebles from "./components/views/ListaInmuebles";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import theme from './theme/theme';
+import AppNavbar from "./components/layout/AppNavbar";
+import Grid from "@material-ui/core/Grid";
 
 class App extends Component{
   render(){
     return(
-    <MuiThemeProvider theme={theme}>
-      <ListaInmuebles/>
-    </MuiThemeProvider>
+      <Router>
+        <MuiThemeProvider theme={theme}>
+          <AppNavbar/>
+            <Grid container>
+
+              <Switch>
+                <Route path="/" exact component={ListaInmuebles}></Route>
+              </Switch>
+
+            </Grid>
+        </MuiThemeProvider>
+      </Router>
     )
   }
 }
-
+ 
 export default App;
---
+++ src/components/layout/AppNavbar.js
@@ -0,0 +1,15 @@
+import React, { Component , Fragment} from 'react'
+import AppBar from "@material-ui/core/AppBar";
+import BarSession from './bar/BarSession';
+
+export default class componentName extends Component {
+    render() {
+        return (
+            <Fragment>
+                <AppBar position = 'static'>
+                    <BarSession/>
+                </AppBar>
+            </Fragment>
+        )
+    }
+}
